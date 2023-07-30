3273cf1c64a6ef9b0bd8b6fd3bc6d606bfe38dd8
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-07-30 03:08:33 +0200

    oupsi

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Gallery from "@/components/Gallery";
+import Image from "next/image";
 import { getEvent } from "@/utils/getEvent";
 
 export default async function EventPage({
@@ -16,7 +16,23 @@ export default async function EventPage({
           <h1 className="font-semibold text-2xl w-fit relative after:absolute after:bg-orange-600 after:w-full after:h-1 after:-bottom-1.5 after:left-2 after:rounded-full">
             {event.title}
           </h1>
-          <Gallery photos={event.photos} />
+          <ul className="mt-8 grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {event.photos.map((photo) => (
+              <Image
+                key={photo.id}
+                className={
+                  photo.width < photo.height
+                    ? "row-span-2 w-full h-full object-cover rounded-md"
+                    : "w-full h-full object-cover rounded-md"
+                }
+                src={photo.url}
+                width={photo.width}
+                height={photo.height}
+                alt={photo.name}
+                quality={30}
+              />
+            ))}
+          </ul>
         </>
       ) : (
         <></>
