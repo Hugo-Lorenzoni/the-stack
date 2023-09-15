7a485c52bad572c29b63a98004b29ff6a8f15dcf
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-09-15 10:51:50 +0200

    fix images

---
+++ src/app/api/images/[...path]/route.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import path from "path";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { path: string[] } }
+) {
+  const filePath = path.resolve(
+    ".",
+    `public/${params.path[0]}/${params.path[1]}/${params.path[2]}`
+  );
+  const imageBuffer = fs.readFileSync(filePath);
+  return new Response(imageBuffer);
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,52 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      // `/admin` requires admin role
-      // console.log(token?.role);
+export default withAuth(
+  function middleware(req) {
+    if (
+      req.nextUrl.pathname.startsWith("/BAPTISE") ||
+      req.nextUrl.pathname.startsWith("/OUVERT") ||
+      req.nextUrl.pathname.startsWith("/AUTRE") ||
+      req.nextUrl.pathname.startsWith("/SPONSORS")
+    ) {
       // console.log(req.nextUrl.pathname);
+      const url = req.nextUrl.clone();
+      url.pathname = `/api/images${req.nextUrl.pathname}`;
+      // console.log(url.pathname);
 
-      if (
-        req.nextUrl.pathname.startsWith("/admin") ||
-        req.nextUrl.pathname.startsWith("/api/admin")
-      ) {
-        return token?.role === "ADMIN";
-      }
-      if (
-        req.nextUrl.pathname.startsWith("/fpmsevents") ||
-        req.nextUrl.pathname.startsWith("/api/fpmsevents") ||
-        req.nextUrl.pathname.startsWith("/BAPTISE") ||
-        req.nextUrl.pathname.startsWith("/videos")
-      ) {
-        return token?.role === "BAPTISE" || token?.role === "ADMIN";
-      }
-      // `/events/:path*` only requires the user to be logged in
-      return !!token;
-    },
-    //authorized: ({ req, token }) => token?.role === "admin",
+      return NextResponse.rewrite(url);
+    }
+
+    // console.log(req.nextauth.token);
   },
-});
+  {
+    callbacks: {
+      authorized({ req, token }) {
+        // `/admin` requires admin role
+        // console.log(token?.role);
+        // console.log(req.nextUrl.pathname);
+
+        if (
+          req.nextUrl.pathname.startsWith("/admin") ||
+          req.nextUrl.pathname.startsWith("/api/admin")
+        ) {
+          return token?.role === "ADMIN";
+        }
+        if (
+          req.nextUrl.pathname.startsWith("/fpmsevents") ||
+          req.nextUrl.pathname.startsWith("/api/fpmsevents") ||
+          req.nextUrl.pathname.startsWith("/BAPTISE") ||
+          req.nextUrl.pathname.startsWith("/videos")
+        ) {
+          return token?.role === "BAPTISE" || token?.role === "ADMIN";
+        }
+        // `/events/:path*` only requires the user to be logged in
+        return !!token;
+      },
+      //authorized: ({ req, token }) => token?.role === "admin",
+    },
+  }
+);
 
 export const config = {
   matcher: [
