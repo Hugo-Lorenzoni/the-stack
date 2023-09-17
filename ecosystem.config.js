404bb82a59358e5946ec8324eca4d8fc6b8f58ff
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-09-17 05:08:11 +0200

    cluster 4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,14 @@ module.exports = {
       cwd: rootPath,
       script: "node_modules/next/dist/bin/next",
       args: "start",
-      instances: 0,
+      instances: 4,
       exec_mode: "cluster",
       watch: false,
       merge_logs: true,
       env: {
         NODE_ENV: "production",
       },
+      node_args: "max-old-space-size=1024",
     },
   ],
 };
