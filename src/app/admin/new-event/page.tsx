dd0af6bb0e60a38195b9f97db024281d15906a78
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-09-17 05:38:23 +0200

    fix for date

--- src/app/admin/new-event/page.tsx
+++ src/app/admin/new-event/page.tsx
@@ -163,6 +163,7 @@ export default function NewEventPage() {
       formData.append("file", values.photos[index]);
     }
     const { cover, photos, ...data } = values;
+    values.date.setDate(values.date.getDate() + 1);
 
     formData.append("values", JSON.stringify(data));
     console.log(formData);
