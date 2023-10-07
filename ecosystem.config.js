00b2581fb58f3ed1b0616d87248b064de46517c6
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-10-07 17:34:56 +0200

    timestamp on logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,8 @@ module.exports = {
       name: "site-cpv",
       cwd: rootPath,
       script: "node_modules/next/dist/bin/next",
+      time: true,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
       args: "start",
       instances: 4,
       exec_mode: "cluster",
