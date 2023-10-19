e3d538cb7f5dcd88f5ecf4ac853189844dc631c5
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-10-19 23:48:09 +0200

    sticky admin nav

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -19,114 +19,118 @@ export default function AdminLayout({
 
   return (
     <div className="flex min-h-[calc(100vh_-_10rem)] flex-col lg:flex-row">
-      <aside className="flex w-full snap-x snap-mandatory scroll-ml-0 scroll-px-2 flex-row gap-2 overflow-x-scroll whitespace-nowrap p-2 font-semibold lg:w-fit lg:min-w-fit lg:flex-col lg:overflow-auto lg:p-4 lg:shadow-2xl">
-        <Link
-          className={currentRoute === "/admin" ? activeStyle : linkStyle}
-          href="/admin"
-        >
-          Tableau de bord
-        </Link>
-        <ResponsiveSeparator />
-        <Link
-          className={
-            currentRoute === "/admin/new-event" ? activeStyle : linkStyle
-          }
-          href="/admin/new-event"
-        >
-          Créer un nouvel événement
-        </Link>
-        <Link
-          className={
-            currentRoute === "/admin/drafted-events" ? activeStyle : linkStyle
-          }
-          href="/admin/drafted-events"
-        >
-          Publication des événements
-        </Link>
-        <Link
-          className={
-            currentRoute.includes("/admin/events-management")
-              ? activeStyle
-              : linkStyle
-          }
-          href="/admin/events-management"
-        >
-          Gestion des événements
-        </Link>
-        <ResponsiveSeparator />
-        <Link
-          className={
-            currentRoute === "/admin/accounts-approval"
-              ? activeStyle
-              : linkStyle
-          }
-          href="/admin/accounts-approval"
-        >
-          Approbation des comptes
-        </Link>
-        <Link
-          className={
-            currentRoute === "/admin/accounts-management"
-              ? activeStyle
-              : linkStyle
-          }
-          href="/admin/accounts-management"
-        >
-          Gestion des comptes
-        </Link>
-        <ResponsiveSeparator />
-        <Link
-          className={
-            currentRoute === "/admin/new-sponsor" ? activeStyle : linkStyle
-          }
-          href="/admin/new-sponsor"
-        >
-          Créer un nouveau sponsor
-        </Link>
-        <Link
-          className={
-            currentRoute === "/admin/sponsors-management"
-              ? activeStyle
-              : linkStyle
-          }
-          href="/admin/sponsors-management"
-        >
-          Gestion des sponsors
-        </Link>
-        <ResponsiveSeparator />
-        <Link
-          className={
-            currentRoute === "/admin/new-video" ? activeStyle : linkStyle
-          }
-          href="/admin/new-video"
-        >
-          Nouvelle vidéo
-        </Link>
-        <Link
-          className={
-            currentRoute === "/admin/videos-management"
-              ? activeStyle
-              : linkStyle
-          }
-          href="/admin/videos-management"
-        >
-          Gestion des vidéos
-        </Link>
-        <ResponsiveSeparator />
-        <Link
-          className={currentRoute === "/admin/comite" ? activeStyle : linkStyle}
-          href="/admin/comite"
-        >
-          Comité
-        </Link>
-        <Link
-          className={
-            currentRoute === "/admin/text-intro" ? activeStyle : linkStyle
-          }
-          href="/admin/text-intro"
-        >
-          Texte d&apos;introduction
-        </Link>
+      <aside className="sticky top-20 z-10 flex lg:shadow-2xl">
+        <ul className="sticky top-20 flex h-max w-full snap-x snap-mandatory scroll-ml-0 scroll-px-2 flex-row gap-2 overflow-x-scroll whitespace-nowrap bg-white p-2 font-semibold lg:w-fit lg:min-w-fit lg:flex-col lg:overflow-auto lg:p-4 ">
+          <Link
+            className={currentRoute === "/admin" ? activeStyle : linkStyle}
+            href="/admin"
+          >
+            Tableau de bord
+          </Link>
+          <ResponsiveSeparator />
+          <Link
+            className={
+              currentRoute === "/admin/new-event" ? activeStyle : linkStyle
+            }
+            href="/admin/new-event"
+          >
+            Créer un nouvel événement
+          </Link>
+          <Link
+            className={
+              currentRoute === "/admin/drafted-events" ? activeStyle : linkStyle
+            }
+            href="/admin/drafted-events"
+          >
+            Publication des événements
+          </Link>
+          <Link
+            className={
+              currentRoute.includes("/admin/events-management")
+                ? activeStyle
+                : linkStyle
+            }
+            href="/admin/events-management"
+          >
+            Gestion des événements
+          </Link>
+          <ResponsiveSeparator />
+          <Link
+            className={
+              currentRoute === "/admin/accounts-approval"
+                ? activeStyle
+                : linkStyle
+            }
+            href="/admin/accounts-approval"
+          >
+            Approbation des comptes
+          </Link>
+          <Link
+            className={
+              currentRoute === "/admin/accounts-management"
+                ? activeStyle
+                : linkStyle
+            }
+            href="/admin/accounts-management"
+          >
+            Gestion des comptes
+          </Link>
+          <ResponsiveSeparator />
+          <Link
+            className={
+              currentRoute === "/admin/new-sponsor" ? activeStyle : linkStyle
+            }
+            href="/admin/new-sponsor"
+          >
+            Créer un nouveau sponsor
+          </Link>
+          <Link
+            className={
+              currentRoute === "/admin/sponsors-management"
+                ? activeStyle
+                : linkStyle
+            }
+            href="/admin/sponsors-management"
+          >
+            Gestion des sponsors
+          </Link>
+          <ResponsiveSeparator />
+          <Link
+            className={
+              currentRoute === "/admin/new-video" ? activeStyle : linkStyle
+            }
+            href="/admin/new-video"
+          >
+            Nouvelle vidéo
+          </Link>
+          <Link
+            className={
+              currentRoute === "/admin/videos-management"
+                ? activeStyle
+                : linkStyle
+            }
+            href="/admin/videos-management"
+          >
+            Gestion des vidéos
+          </Link>
+          <ResponsiveSeparator />
+          <Link
+            className={
+              currentRoute === "/admin/comite" ? activeStyle : linkStyle
+            }
+            href="/admin/comite"
+          >
+            Comité
+          </Link>
+          <Link
+            className={
+              currentRoute === "/admin/text-intro" ? activeStyle : linkStyle
+            }
+            href="/admin/text-intro"
+          >
+            Texte d&apos;introduction
+          </Link>
+        </ul>
       </aside>
       <main className="my-8 flex grow flex-col overflow-x-auto px-4 md:px-14">
         <h1 className="text-2xl font-semibold">Admin Dashboard</h1>
