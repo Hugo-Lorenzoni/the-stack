09c2b0abee536ef3d97d20e7d74b00143e4e2619
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-10-29 14:09:55 +0100

    video search

--- src/components/VideosPagination.tsx
+++ src/components/VideosPagination.tsx
@@ -3,6 +3,8 @@
 import { Video } from "@prisma/client";
 import { useState } from "react";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
 
 type PropsType = {
   videos: Video[];
@@ -10,15 +12,30 @@ type PropsType = {
 
 export default function VideosPagination({ videos }: PropsType) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [filteredVideos, setFilteredVideos] = useState(videos);
 
   const videosPerPage = 6;
 
   const start = (Number(currentPage) - 1) * Number(videosPerPage); // 0, 5, 10 ...
   const end = start + Number(videosPerPage); // 5, 10, 15 ...
-  const results = videos.slice(start, end);
+  const results = filteredVideos.slice(start, end);
 
   return (
     <>
+      <div className="relative max-w-md lg:col-span-2">
+        <Input
+          className="pr-12"
+          onChange={(e) =>
+            setFilteredVideos(
+              videos.filter((videos) => videos.name.includes(e.target.value)),
+            )
+          }
+          placeholder="Search"
+        />
+        <div className="pointer-events-none absolute bottom-0 right-0 top-0 flex items-center rounded-lg px-4">
+          <Search className="h-4 w-4" />
+        </div>
+      </div>
       {results.map((video) => (
         <div key={video.id}>
           <h2 className="mb-2 font-semibold">{video.name}</h2>
