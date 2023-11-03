11b115c76382237a07d60e8d5243135d0525132a
Hugo-Lorenzoni <82056768+Hugo-Lorenzoni@users.noreply.github.com>
2023-11-03 15:50:47 +0100

    disable richcolors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <Providers>
           <Nav />
           {children}
-          <Toaster richColors closeButton />
+          <Toaster closeButton />
           <Footer />
         </Providers>
       </body>
