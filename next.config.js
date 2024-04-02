2903ada72c6807939e5528410e7389f6fced04b7
Hugo Lorenzoni <210937@umons.ac.be>
2024-04-02 21:03:45 +0200

    docker config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     // limit of 25 deviceSizes values
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
