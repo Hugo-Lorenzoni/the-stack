1ac2e7490a715faf764d0d28ae1b8d76eed4d080
Hugo Lorenzoni <210937@umons.ac.be>
2025-08-13 01:13:06 +0200

    fix: improve photo deletion logic and handle missing files

--- src/app/api/admin/deletephoto/route.ts
+++ src/app/api/admin/deletephoto/route.ts
@@ -1,18 +1,42 @@
 import prisma from "@/lib/prisma";
 import { Photo } from "@prisma/client";
+import { existsSync } from "fs";
 import { unlink } from "fs/promises";
 import { NextResponse } from "next/server";
-import { join } from "path";
+import path, { join } from "path";
 import { env } from "process";
 
 export async function DELETE(request: Request) {
   try {
     const body: Photo = await request.json();
 
-    const path = join(env.DATA_FOLDER, "photos", body.url);
-    // console.log(await stat(path));
+    const filePath = join(env.DATA_FOLDER, "photos", body.url);
+    // console.log(await stat(filePath));
 
-    await unlink(path);
+    if (!existsSync(filePath)) {
+      return NextResponse.json({ message: "File not found" }, { status: 404 });
+    }
+    await unlink(filePath);
+
+    // Delete the other versions of the photo
+    for (const quality of [
+      "thumbnail",
+      "preview",
+      "full",
+      "placeholder",
+    ] as const) {
+      const qualityPath = path
+        .format({
+          ...path.parse(filePath),
+          base: undefined, // so it uses name + ext instead of base
+          ext: "webp",
+        })
+        .replace(/\.(?=[^.]*$)/, `_${quality}.`);
+
+      if (existsSync(qualityPath)) {
+        await unlink(qualityPath);
+      }
+    }
 
     const result = await prisma.photo.delete({
       where: {
