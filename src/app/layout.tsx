828a248d4ec719765b990017a3f547e14f4c2fba
Hugo Lorenzoni <210937@umons.ac.be>
2025-08-23 15:28:26 +0200

    feat: add prod deployment workflow and not indexing test website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import Providers from "@/components/Providers";
 import { Toaster } from "sonner";
+import { env } from "node:process";
 
 const inter = Poppins({
   subsets: ["latin"],
@@ -16,6 +17,12 @@ export const metadata: Metadata = {
   title: "CPV FPMs",
   description:
     "Photos des événements de la Faculté Polytechnique de Mons (FPMs)",
+  ...(env.NEXTAUTH_URL === "https://cpv-test.magellan.fpms.ac.be/" && {
+    robots: {
+      index: false,
+      follow: false,
+    },
+  }),
 };
 
 const toastOptions = { duration: 10000 };
