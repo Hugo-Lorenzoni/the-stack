871ca23a19a327049cfdaed38d40d92a0cb09157
Hugo Lorenzoni <210937@umons.ac.be>
2025-08-24 00:12:32 +0200

    fix: simplify middleware by removing unused routes and cleaning up code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,66 +1,36 @@
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(req) {
-    if (
-      req.nextUrl.pathname.startsWith("/BAPTISE") ||
-      req.nextUrl.pathname.startsWith("/OUVERT") ||
-      req.nextUrl.pathname.startsWith("/AUTRE") ||
-      req.nextUrl.pathname.startsWith("/SPONSORS")
-    ) {
-      // console.log(req.nextUrl.pathname);
-      const url = req.nextUrl.clone();
-      url.pathname = `/api/images${req.nextUrl.pathname}`;
-      // console.log(url.pathname);
-
-      return NextResponse.rewrite(url);
-    }
-
-    // console.log(req.nextauth.token);
-  },
-  {
-    callbacks: {
-      authorized({ req, token }) {
-        // `/admin` requires admin role
-        // console.log(token?.role);
-        // console.log(req.nextUrl.pathname);
-
-        if (
-          req.nextUrl.pathname.startsWith("/admin") ||
-          req.nextUrl.pathname.startsWith("/api/admin")
-        ) {
-          return token?.role === "ADMIN";
-        }
-        if (
-          req.nextUrl.pathname.startsWith("/fpmsevents") ||
-          req.nextUrl.pathname.startsWith("/api/fpmsevents") ||
-          req.nextUrl.pathname.startsWith("/BAPTISE") ||
-          req.nextUrl.pathname.startsWith("/videos")
-        ) {
-          return token?.role === "BAPTISE" || token?.role === "ADMIN";
-        }
-        // `/events/:path*` only requires the user to be logged in
-        return !!token;
-      },
-      //authorized: ({ req, token }) => token?.role === "admin",
+export default withAuth(function middleware(req) {}, {
+  callbacks: {
+    authorized({ req, token }) {
+      if (
+        req.nextUrl.pathname.startsWith("/admin") ||
+        req.nextUrl.pathname.startsWith("/api/admin")
+      ) {
+        return token?.role === "ADMIN";
+      }
+      if (
+        req.nextUrl.pathname.startsWith("/fpmsevents") ||
+        req.nextUrl.pathname.startsWith("/videos") ||
+        req.nextUrl.pathname.startsWith("/api/image/BAPTISE")
+      ) {
+        return token?.role === "BAPTISE" || token?.role === "ADMIN";
+      }
+      return !!token;
     },
   },
-);
+});
 
 export const config = {
   matcher: [
     "/admin/:path*",
-    "/fpmsevents/:path*",
-    "/events/:path*",
     "/api/admin/:path*",
-    "/api/fpmsevents/:path*",
-    "/OUVERT/:path*",
-    "/BAPTISE/:path*",
-    "/AUTRE/:path*",
-    "/SPONSORS/:path*",
+    "/fpmsevents/:path*",
+    "/api/image/BAPTISE/:path*",
+    "/videos/:path*",
+    "/events/:path*",
+    "/api/image/OUVERT/:path*",
     "/search/:path*",
     "/api/search/:path*",
-    "/videos/:path*",
   ],
 };
