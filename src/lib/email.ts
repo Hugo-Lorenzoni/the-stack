c1b3df02ae883d64471e2f774097629e6a69c11d
Hugo-Lorenzoni <hugo.lorenzoni@necko.tech>
2025-11-15 15:48:13 +0100

    fix: add GOOGLE_ACCESS_TOKEN to email transport configuration

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,12 +2,15 @@ import nodemailer from "nodemailer";
 import { env } from "process";
 
 export const nodemailerClient = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     type: "OAuth2",
     user: env.EMAIL,
     clientId: env.GOOGLE_CLIENT_ID,
     clientSecret: env.GOOGLE_CLIENT_SECRET,
     refreshToken: env.GOOGLE_REFRESH_TOKEN,
+    accessToken: env.GOOGLE_ACCESS_TOKEN,
   },
 });
